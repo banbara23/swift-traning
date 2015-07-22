d1b8087e11a7f11431ba689456b04d2ae49f90cd
banbara23 <banbara.work@gmail.com>
2015-07-23 08:48:20 +0900

    first commit

---
+++ swift-traning/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift-traning
+//
+//  Created by banbaraniisan on 2015/07/23.
+//  Copyright (c) 2015年 ikemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("Hellow World!")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
